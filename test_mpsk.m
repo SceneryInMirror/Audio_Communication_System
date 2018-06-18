b3089d20c92f30d76bc0283e5e3aa40a885a7ee8
ykx@ESDC <ykx150905@gmail.com>
2018-06-18 22:54:42 +0800

    2018.06.18

--- test_mpsk.m
+++ test_mpsk.m
@@ -1,174 +1,274 @@
 %% Audio Input
 
-%wav = input('Plase input the filename of the audio: \n', 's');
-
-% sample: datapoints of the audio
-% fs: frequency of samplerate
-% nbits: bits of sampling 
-%[sample, fs, nbits] = wavread(wav);
-
-% cnt_point: number of datapoints
-% cnt_track: number of tunnels
-% delta_t: interval of TD
-% t: sequence of time for sampling
-%[cnt_point, cnt_track] = size(sample);
-%delta_t = 1 / fs;
-%t = (0:1:cnt_point = 1) / fs;
-
+%clear
 M = 8;
 K = log2(M);
 sym_map=[1;(1+1i)/sqrt(2);1i;(-1+1i)/sqrt(2);-1;(-1-1i)/sqrt(2);-1i;(1-1i)/sqrt(2)]; %8PSK symbols
 
+% sample: datapoints of the audio
+% fs: frequency of samplerate
+% cnt_point: number of datapoints
+% cnt_track: number of tunnels
+basepath = '/home/ykx/Audio_Communication_System/';
+wav = [basepath 'music.wav'];
+[sample, fs] = audioread(wav, 'native');
+info = audioinfo(wav);
+[cnt_point, cnt_track] = size(sample);
+
+%sample = sample(:,1); % only use mono-channel statics
+sample = reshape(sample, [], 1);
+%sample = repmat(sample, [2,1]);
+bits = reshape(double(dectobin(sample, 16)) - 48, 3, []);
+Ns = length(bits);
+BER = [];   
+theory = [];
+
+% display & save
+figure(1)
+subplot(3,1,1)
+plot(sample)
+title('Audio\_TD');
+subplot(3,1,2)
+plot(abs(fft(sample)))
+title('Audio\_FD');
+subplot(3,1,3)
+plot(reshape(bits, 1, []))
+title('Generated Binary Sequence');
+saveas(1, [basepath 'figure/audio.png']);
+
+
+
 %% Generate Random Sequence
 
-Ns = 10;              % Number of symbols, strongly influence the CPU, max 10000
-bits = [
-     0     0     1     1     0     1     0     1     1     0;
-     0     0     0     0     1     1     0     0     0     0;
-     1     1     1     1     1     1     1     0     1     1];% For test
+%Ns = 100;              % Number of symbols, strongly influence the CPU, max 10000
+%bits = [
+%     0     0     1     1     0     1     0     1     1     0;
+%     0     0     0     0     1     1     0     0     0     0;
+%     1     1     1     1     1     1     1     0     1     1];% For test
 %bits = round(rand(K,Ns));           % KxNs matrix of random 0,1 bits
- 
+
+
 %% Generate 8PSK Signal
 
-% WAY 1:
-Nb = 100; % point number of carrier for one period, strongly influence the scatterplot
 fc = 1; % frequency of carrier, also the frequency of symbol
-t = 0:1/(Nb * fc):1/fc - 1/(Nb * fc); % time sequence for a period
-%carrier = exp(1i * 2 * pi * fc * t);
-carrier = sign(t + 1);
 
 s_mpsk = [];
 s_I = [];
 s_Q = [];
-test = [];
 
-for n=1:Ns
-    k = 4 * bits(1, n) + 2 * bits(2, n) + bits(3, n) + 1;
-    test = [test k];
-    s_mpsk = [s_mpsk real(sym_map(k) * carrier)]; % generate 8PSK signal
-    s_I = [s_I real(sym_map(k))]; % the I branch of 8PSK
-    s_Q = [s_Q imag(sym_map(k))]; % the Q branch of 8PSK
-end
+k = 4 * bits(1, :) + 2 * bits(2, :) + bits(3, :) + 1;
+s_mpsk = [s_mpsk sym_map(k)];
+s_I = [s_I real(sym_map(k))]; % the I branch of 8PSK
+s_Q = [s_Q imag(sym_map(k))]; % the Q branch of 8PSK
+s = [s_I s_Q];
 
-figure(3)
-subplot(4,1,1)
-plot(s_mpsk)
-subplot(4,1,2)
-plot(abs(fft(s_mpsk)))
-subplot(4,1,3)
-plot(s_I)
-subplot(4,1,4)
-plot(s_Q)
+% display and save
+figure(2)
+subplot(2,1,1)
+stem(s_I, '.')
+title('Signal\_I')
+subplot(2,1,2)
+stem(s_Q, '.')
+title('Signal\_Q')
+saveas(2, [basepath 'figure/8psk.png']);
 
-% WAY 2:
-%[s_Q, s_I] = mpsk(bits, Ns);
-%subplot(2,2,3)
-%plot(s_Q);
-%subplot(2,2,4)
-%plot(s_I);
-%mpsk_signal = s_I - s_Q;
-%figure(2)
-%subplot(2,1,1)
-%plot(mpsk_signal);
-%subplot(2,1,2)
-%plot(abs(fft(mpsk_signal)));
 
 %% Upsample
 
-s_upsample_I = upsample(s_I, 8); % 8 times upsample
-s_upsample_Q = upsample(s_Q, 8); % 8 times upsample
+s_upsample = upsample(s, 8);
 
-figure(4)
-subplot(2,1,1)
-plot(s_upsample_I);
-subplot(2,1,2)
-plot(abs(fft(s_upsample_I)));
+% display and save
+figure(3)
+subplot(4,1,1)
+stem(s_upsample(:,1), '.');
+title('Upsample\_I\_TD')
+subplot(4,1,2)
+stem(abs(fft(s_upsample(:,1))), '.');
+title('Upsample\_I\_FD')
+subplot(4,1,3)
+stem(s_upsample(:,2), '.');
+title('Upsample\_Q\_TD')
+subplot(4,1,4)
+stem(abs(fft(s_upsample(:,2))), '.');
+title('Upsample\_Q\_FD')
+saveas(3, [basepath 'figure/upsample.png']);
+
 
 %% Lowpass Filter
 
-%s_transmit = lowpass_transmit(s_upsample);
-s_transmit_I = s_upsample_I; % for test
-s_transmit_Q = s_upsample_Q; % for test
+hd = HD;
+myfilter = hd.Numerator;
+s_transmit = filter(myfilter, 1, [s_upsample; zeros((length(myfilter) - 1) / 2, 2)]);
+s_transmit = s_transmit(((length(myfilter)-1)/2+1): end, 1:2);
+
+% display and save
+figure(4)
+subplot(2,1,1)
+plot(s_transmit);
+legend('signal\_I', 'signal\_Q');
+title('Shaping\_TD')
+subplot(2,1,2)
+%stem(abs(fft(s_transmit)), '.');
+plot(abs(fft(s_transmit)));
+legend('signal\_I', 'signal\_Q');
+title('Shaping\_FD')
+saveas(4, [basepath 'figure/shaping.png']);
+
+% combine two branches
+s_transmit = s_transmit(:,1) + 1i * s_transmit(:,2);
+
 
 %% AWGN Channel
 
-%Es = 10.^([[-7] [8:1:22]]/10); % Energy per symbol
-Es = 10; %for test
-Eb = Es/K;                % Energy per bit
-N0 = 2;
-SNR = 10 * log10((K * Eb/N0) / (8 * fc));
-s_awgn_I = awgn(s_transmit_I, SNR, 'measured');
-s_awgn_Q = awgn(s_transmit_Q, SNR, 'measured');
+Es_range = 10.^([[-7] [8:1:22]]/10); % Energy per symbol
+%Es = 10; %for test
+for item = 1:16
+    Es = Es_range(item);
+    Eb = Es/K;                % Energy per bit
+    N0 = 2;
+    SNR = 10 * log10((K * Eb/N0) / 8);
+    s_awgn = awgn(s_transmit, SNR, 'measured');
+    
+    % display and save
+    figure(5)
+    subplot(2,1,1)
+    plot(abs(s_awgn), 'b');
+    hold on;
+    plot(abs(s_transmit), 'r');
+    legend('after\_awgn', 'before\_awgn');
+    title('AWGN\_Amplitude');
+    hold off;
+    subplot(2,1,2)
+    plot(angle(s_awgn), 'b');
+    hold on;
+    plot(angle(s_transmit), 'r');
+    legend('after\_awgn', 'before\_awgn');
+    title('AWGN\_Angle');
+    hold off;
+    saveas(5, fullfile(basepath, 'figure/', string(item), '/awgn.png'));
+    
+    %% Lowpass Filter
 
-figure(5)
-subplot(2,1,1)
-plot(s_awgn_I);
-subplot(2,1,2)
-plot(abs(fft(s_awgn_I)));
-
-%% Lowpass Filter
-
-%s_receive = lowpass_transmit(s_awgn);
-s_receive_I = s_awgn_I; %for test
-s_receive_Q = s_awgn_Q; %for test
-
-%% Downsample
-
-s_downsample_I = downsample(s_receive_I, 8); % 8 times downsample
-s_downsample_Q = downsample(s_receive_Q, 8); % 8 times downsample
-
-
-%% 8PSK Judgement
-
-s_demodulate_I = s_downsample_I;
-s_demodulate_Q = s_downsample_Q;
-
-
-figure(6)
-subplot(2,1,1)
-plot(s_demodulate_I);
-subplot(2,1,2)
-plot(s_demodulate_Q);
-
-figure(7)
-plot(s_demodulate_I, s_demodulate_Q, 'b.')
-
-s_result = s_demodulate_I - 1i * s_demodulate_Q;
-distance = abs(repmat(s_result, [M, 1]) - repmat(sym_map, [1, Ns]));
-
-[min_dis, min_pos] = min(uint32(distance .* 10000));
-
-figure(8)
-subplot(3,1,1)
-plot(test)
-subplot(3,1,2)
-plot(min_pos)
-
-subplot(3,1,3)
-plot(test-min_pos)
-
-min_pos = min_pos - 1;
-bits_result = [];
-bits_result = [bits_result sign(bitand(min_pos, 4))];
-bits_result = [bits_result; sign(bitand(min_pos, 2))];
-bits_result = [bits_result; mod(min_pos, 2)];
-
-%% Calculate
+    %s_receive = real(s_awgn);
+    %s_receive = [s_receive imag(s_awgn)];
+    %s_receive = s_awgn;
+    %s_receive = filter(myfilter, 1, [s_receive; zeros((length(myfilter) - 1) / 2, 2)]);
+    %s_receive = s_receive(((length(myfilter)-1)/2+1): end, 1:2);
+    s_receive = filter(myfilter, 1, [s_awgn; zeros((length(myfilter) - 1) / 2, 1)]);
+    s_receive = s_receive(((length(myfilter)-1)/2+1): end, 1);
+    %s_receive = real(s_awgn);
+    %s_receive = [s_receive imag(s_awgn)];
+    
+    figure(6)
+    subplot(3,1,1)
+    plot(abs(s_receive), 'b');
+    title('after filter');
+    subplot(3,1,2)
+    plot(abs(s_awgn), 'r');
+    title('after awgn');
+    subplot(3,1,3)
+    plot(abs(s_transmit), 'y');
+    title('before awgn');
+    saveas(6, fullfile(basepath, 'figure/', string(item), '/matchedfilter.png'));
 
 
+    %% Downsample
+
+    s_downsample = downsample(s_receive, 8);
+    s_downsample = [real(s_downsample) imag(s_downsample)];
+    
+    figure(7)
+    %subplot(2,1,1)
+    periodogram(s_downsample);
+    %stem(s_downsample, '.');
+    title('Downsample\_pdf');
+    %subplot(2,1,2)
+    %stem(abs(fft(s_downsample)), '.');
+    %title('Downsample\_FD');
+    saveas(7, fullfile(basepath, 'figure/', string(item), '/downsample.png'));
+
+
+    %% 8PSK Judgement
+
+    %s_demodulate_I = s_downsample_I;
+    %s_demodulate_Q = s_downsample_Q;
+    s_demodulate = s_downsample;
+
+    %figure(8)
+    %subplot(2,1,1)
+    %plot(s_demodulate);
+    %subplot(2,1,2)
+    %plot(abs(fft(s_demodulate)));
+
+    figure(8)
+    plot(s_demodulate(:,1), s_demodulate(:,2), 'b.')
+    title('Constellation Diagram');
+    saveas(8, fullfile(basepath, 'figure/', string(item), '/constellation.png'));
+
+    s_result = s_demodulate(:,1) - 1i * s_demodulate(:,2);
+    s_result = s_result';
+    distance = abs(repmat(s_result, [M, 1]) - repmat(sym_map, [1, Ns]));
+
+    [min_dis, min_pos] = min(uint32(distance .* 10000));
+
+    figure(9)
+    subplot(3,1,1)
+    plot(k)
+    subplot(3,1,2)
+    plot(min_pos)
+    subplot(3,1,3)
+    plot(k-min_pos)
+    saveas(9, fullfile(basepath, 'figure/', string(item), '/error.png'));
+
+    min_pos = min_pos - 1;
+    bits_result = [];
+    bits_result = [bits_result sign(bitand(min_pos, 4))]    ;
+    bits_result = [bits_result; sign(bitand(min_pos, 2))];
+    bits_result = [bits_result; mod(min_pos, 2)];
+
+    figure(11)
+    subplot(2,1,1)
+    plot(reshape(bits, 1, []), 'b');
+    title('transmitted\_bits');
+    subplot(2,1,2)
+    plot(reshape(bits_result, 1, []), 'r');
+    title('received\_bits');
+    saveas(11, fullfile(basepath, 'figure/', string(item), '/bits.png'));
+    
+    
+    %% Calculate
+    bits_error = abs(bits_result-bits);
+    %BER = [BER sum(sum(bits_error(:,:) == 1))];
+    BER = [BER biterr(bits, bits_result)];
+    %theory = [theory 1/2 * erfc(1/2*10.^(SNR/10))];
+    
+end
+
+theory = erfc(sqrt(Es_range/N0 * (sin(pi/M))^2));
+figure(10)
+hold off;
+BE = BER;
+BER = BER / (3 * length(bits));
+plot(10 * log10(Es_range/(K * N0)) , 10 * log10(BER), 'b');
+hold on;
+%plot(10 * log10(Es_range/(K * N0)) , 10 * log10(theory), 'r');
+plot(ebno0, 10 * log10(ber0), 'r');
+title('BER(dB)-EbN0(dB)')
+legend('simulate result', 'theoretical calculation');
+saveas(10, [basepath 'figure/BER-EBN0.png']);
 
 %%
-na = length(Es);          % number of energy per symbol
-No = 2;                   % noise unit variance (watt/Hz)
-Es_No = Es/No;              % EsNo
-Eb_No = Eb/No;              % EbNo
+%na = length(Es);          % number of energy per symbol
+%No = 2;                   % noise unit variance (watt/Hz)
+%Es_No = Es/No;              % EsNo
+%Eb_No = Eb/No;              % EbNo
 
-BER = zeros(1,na);
-SER = zeros(1,na);
-Pseint = zeros(1,na);
+%BER = zeros(1,na);
+%SER = zeros(1,na);
+%Pseint = zeros(1,na);
 
-dphi = 0.01*pi/M;                   % interval of $\phi$
-phi = [-pi/M+dphi/2:dphi:pi/M];     % $\phi$
-nphi = length(phi);                 % number of $\phi$
+%dphi = 0.01*pi/M;                   % interval of $\phi$
+%phi = [-pi/M+dphi/2:dphi:pi/M];     % $\phi$
+%nphi = length(phi);                 % number of $\phi$
 
 
